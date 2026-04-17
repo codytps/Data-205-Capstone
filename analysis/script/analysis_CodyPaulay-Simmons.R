6b4f751df8115825678a466d79fcd920847827f4
Cody Paulay Simmons <164425793+codytps@users.noreply.github.com>
2026-04-17 16:45:50 -0400

    Add files via upload

--- analysis/script/analysis_CodyPaulay-Simmons.R
+++ analysis/script/analysis_CodyPaulay-Simmons.R
@@ -8,3 +8,15 @@ analysis_data <- data.frame(Region = c("DC", "Hawaii County"),
                             Income = c(dc_income, hi_income),
                             Poverty = c(dc_poverty, hi_poverty))
 analysis_data
+
+str(analysis_data)
+summary(analysis_data)
+
+# --- Analysis Insights ---
+
+# Washington, D.C. has higher income and earnings than Hawaii County.
+
+# D.C. also has higher unemployment and poverty rates.
+
+# This suggests possible income inequality,
+# where higher earnings are not evenly distributed.
